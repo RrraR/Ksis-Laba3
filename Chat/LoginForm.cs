7e0bb376f72a6e5f6692e861d68377757d13acc4
RrraR <tanja.budnitskay@gmail.com>
2022-04-15 02:32:45 +0300

    lab 3

---
+++ Chat/LoginForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chat;
+
+public partial class LoginForm : Form
+{
+    string _userName = "";
+    
+    public string UserName
+    {
+        get { return _userName; }
+    }
+    public LoginForm()
+    {
+        InitializeComponent();
+        FormClosing += LoginForm_FormClosing;
+        BntEnter.Click += BntEnter_Click;
+    }
+
+    private void BntEnter_Click(object sender, EventArgs e)
+    {
+        _userName = txtUsername.Text.Trim();
+        if (string.IsNullOrEmpty(_userName))
+        {
+            MessageBox.Show("Please enter a username");
+            return;
+        }
+        FormClosing -= LoginForm_FormClosing;
+        Close();
+    }
+    
+    void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        _userName = "";
+    }
+}
